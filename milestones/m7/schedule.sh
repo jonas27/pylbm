85e0ec9238901185da44c289661dfce34bfe2836
jonas27 <jonas.burster@gmail.com>
2022-08-15 09:05:37 +0200

    m7 update

--- milestones/m7/schedule.sh
+++ milestones/m7/schedule.sh
@@ -3,17 +3,20 @@
 # pip install --user --upgrade numpy matplotlib mpi4py
 
 # for nodes = 2
-# not possible for 50 72
-# for i in 2 8 18
-# do
-# ./m7.job 2 $((i*2))
-# done
+# not possible for
+for i in 2 8 18 50 72
+do
+./m7.job 2 $i
+done
 
 # for nodes = 4
-for i in 1 4 9 25 36
-do
-./m7.job 4 $i
-done
+# for i in 1 4 9 25 36
+# do
+# ./m7.job 4 $i
+# done
+
+# ./m7.job 4 25
+# ./m7.job 4 36
 
 # for nodes = 8
 # for i in 2 18
